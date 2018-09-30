4a227f7277b51fe5476ef32d7f9a47c1f13aa380
MininMobile <notminin@pm.me>
2018-09-30 16:58:12 +0100

    generate array in range

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ switch (args[0]) {
 		]));
 	} break;
 
+	case "-r": case "--range": {
+		args.shift();
+
+		let range = args.join(""); 
+		range = range.replace(/ |[a-z]/gi, "");
+		range = range.split("-");
+
+		console.log(utilGENARR(parseInt(range[0]), parseInt(range[1])));
+	} break;
+
 	default:
 		console.log(utilERROR("Invalid argument\n\tSee -h for available commands"));
 }
@@ -18,4 +28,13 @@ function utilINFO(details) { return `compsort [info]\n=>\t${details.join("\n\t")
 function utilMULTISHIFT(array, amount) {
 	for (let i = 0; i < amount; i++)
 		array.shift();
+}
+
+function utilGENARR(rOne, rTwo) {
+	let array = [];
+
+	for (let i = rOne; i < rTwo + 1; i++)
+		array.push(i);
+
+	return array;
 }
